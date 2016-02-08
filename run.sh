a9397f1d6cc613b6a31637d725e8a4e8c00ee844
ryan <hendersonry@gmail.com>
2016-02-08 18:06:56 +0100

    check connection until http listening

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/bin/sh -e
+connect_db() {
+    while ! curl 'http://admin:admin@localhost:3000/'
+    do
+        echo "$(date) - waiting for Grafana to be ready"
+        sleep 5
+    done
+    curl 'http://admin:admin@localhost:3000/api/datasources' -X POST -H 'Content-Type: application/json;charset=UTF-8' --data-binary '{"name": "influx", "type": "influxdb","url": "http://localhost:8086", "access": "proxy", "isDefault": true, "database": "telegraf", "user": "root", "password":"root"}'
+}
+
+connect_db &
 
 chown -R grafana:grafana /var/lib/grafana /var/log/grafana
 
@@ -7,3 +17,4 @@ exec gosu grafana /usr/sbin/grafana-server  \
   --config=/etc/grafana/grafana.ini         \
   cfg:default.paths.data=/var/lib/grafana   \
   cfg:default.paths.logs=/var/log/grafana
+
